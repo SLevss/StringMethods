7f7d5d27be1ed2fcd483018f1bdb82767201e0d3
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-03-24 14:05:06 +0300

    HW #01 (back-end)StringMethods

---
+++ string_tests/src/telran/string_tests/StringMethods.java
@@ -0,0 +1,64 @@
+package telran.string_tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+class StringMethods {
+	@Test
+	void testCharAtMethod() {
+	 assertEquals("W",  "World".charAt(0));
+	}
+
+
+	@Test
+	void testCompareToMethod() {
+		assertEquals(0, "World".compareTo("World"));
+	}
+
+	
+
+	@Test
+	void testCompareToIgnoreCaseMethod() {
+		assertEquals(0, "WORLD".compareToIgnoreCase("world"));
+	}
+
+	
+
+	@Test
+	void testConcatMethod() {
+		assertEquals("Svetlana Doskochinskaia","Svetlana".concat("Doskochinskaia"));
+	}
+
+	
+	@Test
+	void testStartWithMethod() {
+		assertEquals(true, "World".startsWith("wor"));
+	}
+
+	
+
+	@Test
+	void testEndWithMethod() {
+		assertEquals(false, "World".endsWith("wor"));
+	}
+
+	
+
+	@Test
+	void testContainsMethod() {
+		assertEquals(true, "World".contains("wor"));
+	}
+
+	
+
+	@Test
+	void testIndexOfMethod() {
+		assertEquals(11, "this crazy world".indexOf("world"));
+	}
+
+	
+
+	@Test
+	void lastIndexOfMethod() {
+		assertEquals(11, "this crazy world".lastIndexOf("world"));
+	}
+}
